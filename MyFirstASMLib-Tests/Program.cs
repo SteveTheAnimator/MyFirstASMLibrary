a2c6350cb95e9a6a5c998d1db3aa21fcb23a2866
SteveCoolFart <103543324+SteveCoolFart@users.noreply.github.com>
2025-07-24 22:24:10 -0600

    fixed stuff and added more stuff

--- MyFirstASMLib-Tests/Program.cs
+++ MyFirstASMLib-Tests/Program.cs
@@ -1,6 +1,15 @@
 ﻿using System.Runtime.InteropServices;
+using System;
 
 [DllImport("mfasmlib.dll", CallingConvention = CallingConvention.Cdecl)]
 static extern long AddNumbers(long a, long b);
 
-Console.WriteLine(AddNumbers(5, 7));
+[DllImport("mfasmlib.dll", CallingConvention = CallingConvention.Cdecl)]
+static extern long MultiplyNumbers(long a, long b);
+
+[DllImport("mfasmlib.dll", CallingConvention = CallingConvention.Cdecl)]
+static extern long SubtractNumbers(long a, long b);
+
+Console.WriteLine($"AddNumbers(5, 7) = {AddNumbers(5, 7)}");
+Console.WriteLine($"MultiplyNumbers(5, 7) = {MultiplyNumbers(5, 7)}");
+Console.WriteLine($"SubtractNumbers(7, 5) = {SubtractNumbers(7, 5)}");
--- mfasmlib.asm
+++ mfasmlib.asm
@@ -1,15 +1,27 @@
 ; readable enough to which I don't need to add comments.
 
 global AddNumbers
+global MultiplyNumbers
+global SubtractNumbers
 global DllMain
 
 section .text
 
 DllMain:
-    mov eax, 1
+    mov eax, 1  
     ret
 
 AddNumbers:
-    mov rax, rcx
-    add rax, rdx
+    mov rax, rcx  
+    add rax, rdx 
+    ret
+
+MultiplyNumbers:
+    mov rax, rcx 
+    imul rax, rdx   
+    ret
+
+SubtractNumbers:
+    mov rax, rcx 
+    sub rax, rdx
     ret
