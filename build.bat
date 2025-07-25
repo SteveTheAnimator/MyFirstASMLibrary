4bbc4c30980581403e4cb158eea98ecb05ee1c8c
SteveCoolFart <103543324+SteveCoolFart@users.noreply.github.com>
2025-07-24 22:25:15 -0600

    removed C# tests

--- build.bat
+++ build.bat
@@ -1,11 +1,4 @@
 @echo off
-REM 
 set PATH=C:\mingw64\bin;%PATH%
-
-REM 
 nasm -f win64 mfasmlib.asm -o mfasmlib.obj
-
-REM 
-x86_64-w64-mingw32-gcc -shared -o mfasmlib.dll mfasmlib.obj ^
-    -Wl,--output-def,mfasmlib.def,--dll ^
-    -lkernel32 -luser32
+x86_64-w64-mingw32-gcc -shared -o mfasmlib.dll mfasmlib.obj -Wl,--output-def,mfasmlib.def,--dll -lkernel32 -luser32
